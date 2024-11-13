ece25822ddc0edec1775888eb554e20573cb127e
Pp3ng <Pp3ng@outlook.com>
2024-11-14 00:43:55 +0900

    feat: add CodeMirror autocomplete functionality and include show-hint addon

--- js/editor.js
+++ js/editor.js
@@ -1,20 +1,21 @@
 let editor = CodeMirror.fromTextArea(document.getElementById("code"), {
     lineNumbers: true,
     mode: "text/x-c++src",
-    theme: "nord",
     keyMap: "default",
     matchBrackets: true,
     autoCloseBrackets: true,
     indentUnit: 4,
     tabSize: 4,
     indentWithTabs: true,
-    lineWrapping: true
+    lineWrapping: true,
+    extraKeys: {
+        "Ctrl-Space": "autocomplete"
+    }
 });
 
 let assemblyView = CodeMirror(document.getElementById("assembly"), {
     lineNumbers: true,
     mode: "gas",
-    theme: "nord",
     readOnly: true,
     lineWrapping: true
 });
